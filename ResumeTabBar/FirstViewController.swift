a6358af088db986292d2c6d1119d0a23c4a6c411
Alexei <motogp1985@gmail.com>
2019-04-25 14:49:08 +0300

    Initial Commit

---
+++ ResumeTabBar/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ResumeTabBar
+//
+//  Created by home on 4/25/19.
+//  Copyright © 2019 home. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ResumeTabBar/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ResumeTabBar
+//
+//  Created by home on 4/25/19.
+//  Copyright © 2019 home. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
